a775fc02aafad99af5e35cabb70177344486928d
Luka Milicevic <lmilicevicmtk@gmail.com>
2026-08-29 19:07:29 +0200

    replace each month's function with a dynamic placeholder and if/else loop

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views     # dot means import from the same folder
 
 urlpatterns = [
-    path("january", views.january),  # first argument is an URL, second one is the pointer to a view
-    path("february", views.february), 
+    # example: path("january", views.january) -> first argument is an URL, second one is the pointer to a view
+    path("<month>", views.monthly_challenge) # placeholder within <> can be anything
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 # Create your views here.
 
-def january(request):
-    return HttpResponse("Eat no meat for the entire month!")
+# example:
+# def january(request):
+#   return HttpResponse("Eat no meat for the entire month!")
 
-def february(request):
-    return HttpResponse("Walk for at least 20 minutes every day!")
+def monthly_challenge(request, month): # second argument is the placeholder from urls
+    challenge_text = None
+    if month == "january":
+        challenge_text = "Try to walk 5000 steps every day!"
+    elif month == "february":
+        challenge_text = "Do a good deed every day!"
+    elif month == "march":
+        challenge_text = "Practice an instrument for 20 minutes a day!"
+    else:
+          return HttpResponseNotFound("This month isn't supported yet")
+    return HttpResponse(challenge_text)
